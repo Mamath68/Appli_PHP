12db07e93d488d4eff9e576044cf3441b7d4d5f6
Mamath68 <mathieu.stamm@gmail.Com>
2023-03-24 13:37:46 +0100

    Premiere partie fini/début CSS + Fonctions demandé

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ajout Produit</title>
+</head>
+
+<body>
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post">
+        <p>
+            <label>
+                Nom du produit
+                <input type="text" name="name">
+            </label>
+        </p>
+        <p>
+            <label>
+                Prix du produit
+                <input type="number" step="any" name="price">
+            </label>
+        </p>
+        <p>
+            <label>
+                Quantité désirée
+                <input type="number" name="qtt" value="1">
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="submit" name="submit" value="Ajouter le produit">
+            </label>
+        </p>
+    </form>
+</body>
+
+</html>
